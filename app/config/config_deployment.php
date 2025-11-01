84d1313026cab455095703b8a1a4191564a44202
NOGiTTiS <Knight_Darkwing@hotmail.com>
2025-11-01 10:56:11 +0700

    v1.3Deployment

--- app/config/config_deployment.php
+++ app/config/config_deployment.php
@@ -1,16 +1,15 @@
 <?php
 // Database Credentials
-define('DB_HOST', 'db'); // ชื่อ service ของ MySQL ใน docker-compose.yml
-define('DB_USER', 'user');
-define('DB_PASS', 'password');
-define('DB_NAME', 'db_edms');
+define('DB_HOST', 'localhost'); // ชื่อ service ของ MySQL ใน docker-compose.yml
+define('DB_USER', 'krusitti_db');
+define('DB_PASS', 'HpXENgteAbC8CzDuXXrQ');
+define('DB_NAME', 'krusitti_db_edms');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__))); // ได้ Path ไปที่ /var/www/html/app
 
 // URL Root
-define('URLROOT', 'http://localhost:8080'); // URL ของโปรเจกต์
-
+define('URLROOT', 'http://www.xn--12cf1de7cd9byec9b.xn--o3cw4h/edms');  // URL ของโปรเจกต์
 
 // Site Name
 define('SITENAME', 'EDMS - ระบบสารบรรณอิเล็กทรอนิกส์');
